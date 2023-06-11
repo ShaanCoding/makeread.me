1ca88dc86ba8c2bea4f16d0ee74bcd5c787787e2
Shaan <22236218+ShaanCoding@users.noreply.github.com>
2023-06-11 18:59:42 +1000

    React query didn't make sense either

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,21 +1,9 @@
 import React from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import DefaultLayout from "./layouts/Default";
 import Landing from "./pages/Landing";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import DarkLightModeToggle from "./components/DarkLightModeToggle";
 import Editor from "./features/Editor/pages/Editor";
-
-const queryClient = new QueryClient();
-
-const ReactQueryDevtoolsProduction = React.lazy(() =>
-  import("@tanstack/react-query-devtools/build/lib/index.prod.js").then(
-    (d) => ({
-      default: d.ReactQueryDevtools,
-    })
-  )
-);
 
 const router = createBrowserRouter([
   {
@@ -69,16 +57,9 @@ export default function App() {
   }, []);
 
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
       <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} />
-      {showDevtools && (
-        <React.Suspense fallback={null}>
-          <ReactQueryDevtoolsProduction />
-        </React.Suspense>
-      )}
-
       <DarkLightModeToggle />
-    </QueryClientProvider>
+    </>
   );
 }
