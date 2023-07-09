f244d6e6cdf241481f0ffb31ca5919da118d84a5
Furkan Emre Kozan <kozanfurkanemre@gmail.com>
2023-07-09 20:32:57 +0300

    refactor: start configs
    - some dependencies switched to dev dependency
    - 2 scripts created: format and serve
    - prettier config created
    - default files deleted
    - layouts created
    - fonts created

---
+++ readme-generator/src/styles/fonts.ts
@@ -0,0 +1,8 @@
+import { Poppins } from 'next/font/google'
+
+export const poppins = Poppins({
+    subsets: ["latin"],
+    variable: "--font-poppins",
+    weight: "500",
+    display: "swap",
+  })
--- readme-generator/tailwind.config.js
+++ readme-generator/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}',
+
   ],
   theme: {
     extend: {
