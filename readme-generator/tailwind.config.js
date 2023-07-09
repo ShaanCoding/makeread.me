4b42310b80ad6248512e0482642417019807943a
Furkan Emre Kozan <kozanfurkanemre@gmail.com>
2023-07-09 20:34:21 +0300

    feat: tailwind config extended
    - font family extended
    - colors extended
    - letter spacing extended

--- readme-generator/tailwind.config.js
+++ readme-generator/tailwind.config.js
@@ -7,11 +7,24 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "#191919",
+        secondary: "#F4F4F4",
+      },
+      fontFamily:{
+        poppins:["var(--font-poppins)"],
+        
+        
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      letterSpacing:{
+        primary:"-0.005rem",
+        secondary:"-0.13px"
+      }
     },
   },
   plugins: [],
