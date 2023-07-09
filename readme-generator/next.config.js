e8554af43312f2c16a52eef0d4ec169e04d56bab
Furkan Emre Kozan <kozanfurkanemre@gmail.com>
2023-07-09 20:20:01 +0300

    feat: @svgr/webpack added
    now we can directly import .svg files

--- readme-generator/next.config.js
+++ readme-generator/next.config.js
@@ -2,7 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+
+    return config;
+  }
 };
 
 module.exports = nextConfig;
