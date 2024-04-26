6babdbf8331bea3cd13be197644c258842d04f02
Shaan Khan <shaankhan101@gmail.com>
2024-04-26 21:09:59 +1000

    [Shaan] Featured Templates Home Screen (#62)
    * Added Row of Featured Templates
    
    - Added Row of feature templates, tackling Add Row of Featured Templates to Home Screen #41
    
    * Responsiveness Cards
    
    - Added Card Responsiveness on mobile devices

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,15 +3,17 @@ import Demo from "@/components/Home/Demo"
 import KeyFeatures from "@/components/Home/KeyFeatures"
 import Splashscreen from "@/components/Home/Splashscreen"
 import Testimonials from "@/components/Home/Testimonials"
+import TopThreeTemplates from "@/components/Home/TopThreeTemplates"
 
 export default function IndexPage() {
   return (
     <section className="grid items-center gap-6 px-[5%] pb-8 pt-6 md:py-10">
       <Splashscreen />
       <Demo />
+      <TopThreeTemplates />
       <KeyFeatures />
-      <CallToAction />
       <Testimonials />
+      <CallToAction />
     </section>
   )
 }
