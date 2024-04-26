77ad032325b4b3058e4740b423bedf392420fe7c
Shaan <22236218+ShaanCoding@users.noreply.github.com>
2024-04-26 21:07:41 +1000

    Added Row of Featured Templates
    - Added Row of feature templates, tackling Add Row of Featured Templates to Home Screen #41

---
+++ frontend/components/Home/TopThreeTemplates.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { ITemplate, readMeGenerator } from "@/api/generated"
+
+import Card from "@/components/SelectTemplate/Card"
+
+const TopThreeTemplates = () => {
+  const [template, setTemplates] = useState<ITemplate[]>([])
+
+  useEffect(() => {
+    ;(async () => {
+      const templateService: any[] | undefined = await (
+        await new readMeGenerator().template.getTemplate()
+      ).responseObject
+
+      if (!templateService) {
+        return
+      }
+
+      setTemplates(templateService.slice(0, 3))
+    })()
+  }, [])
+
+  return (
+    <div>
+      <div className="mb-12 text-center">
+        <h2 className="text-themeGreen font-semibold">Try them out!</h2>
+        <h1 className="text-6xl font-bold text-white">Featured Templates</h1>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 sm:gap-6 lg:grid-cols-3">
+        {template.map((template: ITemplate, index: number) => (
+          <Card {...template} key={index} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default TopThreeTemplates
