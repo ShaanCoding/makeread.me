a225c15ccaaccecaa94b5ed361193efefe8c3f39
Mathys <mathys.deshaies@icloud.com>
2024-05-05 13:20:41 -0400

    feat: add functions to handle templates with localstorage

--- frontend/app/select-template/page.tsx
+++ frontend/app/select-template/page.tsx
@@ -1,17 +1,55 @@
 "use client"
 
 import { useState } from "react"
+import Link from "next/link"
 import { IPageType, ITemplate } from "@/api/generated"
+import { FilePlus2 } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
 import Card from "@/components/SelectTemplate/Card"
 import SelectTemplateSideBar from "@/components/SelectTemplate/Sidebar/Sidebar"
-import { FilePlus2 } from "lucide-react"
-import Link from "next/link"
 
 export default function IndexPage() {
   const [template, setTemplates] = useState<ITemplate[]>([])
   const [pageType, setPageType] = useState<IPageType>(IPageType.NONE)
+
+  function getTemplateId(template: ITemplate): string {
+    return template.title + template.dateCreated
+  }
+
+  function getFavoriteTemplates(): string[] {
+    const favoriteTemplatesJson = localStorage.getItem("favoriteTemplates")
+    if (favoriteTemplatesJson) {
+      return JSON.parse(favoriteTemplatesJson) as string[]
+    } else {
+      return []
+    }
+  }
+
+  function isFavoriteTemplate(templateId: string): boolean {
+    const favoriteTemplates = getFavoriteTemplates()
+    return favoriteTemplates.includes(templateId)
+  }
+
+  function toggleFavoriteTemplate(templateId: string): void {
+    const favoriteTemplates = getFavoriteTemplates()
+    if (!favoriteTemplates.includes(templateId)) {
+      console.log(template)
+      favoriteTemplates.push(templateId)
+      localStorage.setItem(
+        "favoriteTemplates",
+        JSON.stringify(favoriteTemplates)
+      )
+    } else {
+      const newFavoriteTemplates = favoriteTemplates.filter(
+        (id) => id !== templateId
+      )
+      localStorage.setItem(
+        "favoriteTemplates",
+        JSON.stringify(newFavoriteTemplates)
+      )
+    }
+  }
 
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
@@ -32,15 +70,17 @@ export default function IndexPage() {
               </Link>
             </div>
 
-
             <div className="xl:flex xl:items-center xl:justify-center gap-6">
               <h4 className="text-lg font-semibold mb-2 xl:mb-0">Page Type</h4>
 
               <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
                 {Object.keys(IPageType).map((page: string, index: number) => (
                   <Button
-                    variant={`${pageType === (IPageType as any)[page] ? "default" : "outline"
-                      }`}
+                    variant={`${
+                      pageType === (IPageType as any)[page]
+                        ? "default"
+                        : "outline"
+                    }`}
                     key={index}
                     onClick={() => setPageType((IPageType as any)[page])}
                   >
@@ -50,7 +90,6 @@ export default function IndexPage() {
               </div>
             </div>
           </div>
-
 
           {template.length === 0 && (
             <div className="flex items-center justify-center size-full">
@@ -63,12 +102,19 @@ export default function IndexPage() {
           {template.length !== 0 && (
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-y-6 sm:gap-6 lg:grid-cols-3">
               {template.map((template: ITemplate, index: number) => (
-                <Card {...template} key={index} />
+                <Card
+                  {...template}
+                  key={index}
+                  isFavoriteTemplate={isFavoriteTemplate(
+                    getTemplateId(template)
+                  )}
+                  toggleFavoriteTemplate={toggleFavoriteTemplate}
+                />
               ))}
             </div>
           )}
         </main>
-      </div >
-    </div >
+      </div>
+    </div>
   )
 }
