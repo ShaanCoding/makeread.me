29384001eab062f752c309f712c21850e2ebe32e
Shaan <22236218+ShaanCoding@users.noreply.github.com>
2024-05-05 22:41:54 +1000

    Deleted .VsCode Folder
    - Not setup properly so I thought I'd remove
    - Added MongoDB connection logic

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,7 +13,37 @@ import { openAPIRouter } from './api-docs/openAPIRouter'
 // Middleware
 // Helpers
 import errorHandler from './common/middleware/errorHandler'
-import { PORT } from './common/utils/env'
+import { PORT,
+
+    MONGO_URL_LOCAL,
+    MONGOHOST,
+    MONGOPASSWORD,
+    MONGOPORT,
+    MONGOUSER,
+    PRODUCTION_OR_DEVELOPMENT,
+    Environment,
+ } from './common/utils/env'
+
+import mongoose from "mongoose"
+
+
+let mongoDBConnection: string;
+
+if(PRODUCTION_OR_DEVELOPMENT === Environment.PRODUCTION) {
+    mongoDBConnection = `mongodb://${MONGOUSER!}:${MONGOPASSWORD!}@${MONGOHOST!}:${MONGOPORT!}`;
+} else {
+    mongoDBConnection = MONGO_URL_LOCAL!
+}
+
+// Connect to MongoDB
+mongoose.connect(mongoDBConnection)
+    .then(() => console.log('🚀 MongoDB Connected'))
+    .catch((err) => {
+        console.log('MongoDB connection error. Please make sure MongoDB is running. ' + err)
+        process.exit(1)
+    })
+
+
 
 // Create Express server
 const app: Express = express()
