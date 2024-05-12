a4c3a126e0f018bb9e51807be270dce3643f7cfd
Shaan <22236218+ShaanCoding@users.noreply.github.com>
2024-05-12 23:03:30 +1000

    Created Git Icon States w/ Colors
    - Created Components / Images for Common Git Operations

---
+++ frontend/components/common/Github/ClosedIssueDone.tsx
@@ -0,0 +1,8 @@
+import { CircleCheck } from 'lucide-react'
+import React from 'react'
+
+const ClosedIssueDone = () => {
+    return <CircleCheck color="#ab7df8" />
+}
+
+export default ClosedIssueDone
---
+++ frontend/components/common/Github/PullRequestOpen.tsx
@@ -0,0 +1,9 @@
+import { GitPullRequest } from 'lucide-react'
+import React from 'react'
+
+const PullRequestOpen = () => {
+    return <GitPullRequest color="#3fb950" />
+}
+
+
+export default PullRequestOpen
