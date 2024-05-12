755c9f2593dfbf5df81c6bb96d6785d908d94023
Shaan <22236218+ShaanCoding@users.noreply.github.com>
2024-05-12 23:06:06 +1000

    Removed Logger

--- frontend/components/Contributors/GithubContributionLinks.tsx
+++ frontend/components/Contributors/GithubContributionLinks.tsx
@@ -1,12 +1,12 @@
-import { IGithubLink } from "@/app/contributors/contributions";
+import { IGithubLink } from "@/config/contributions";
 import Link from "next/link";
 import React from "react";
 import { GitPullRequest, CircleDot } from "lucide-react";
 
 export default function GithubContributionLinks({ githubLinks }: { githubLinks: IGithubLink[] }) {
-  console.log(githubLinks)
   return (
     <div className="mb-4">
+
       {
         githubLinks.map((link, key) => {
           return (
@@ -26,9 +26,9 @@ export default function GithubContributionLinks({ githubLinks }: { githubLinks: 
     </div>
   )
 }
-
+// TODO: Remove
 function Icon({ type }: { type: string }) {
   return (
-    type === "PR" ? <GitPullRequest color="#00D100" /> : <CircleDot color="#00D100" />
+    type === "PR" ? <GitPullRequest color="#3fb950" /> : <CircleDot color="#3fb950" />
   )
 }
