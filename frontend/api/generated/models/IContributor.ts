b6bf7fc69b1e57b5882a5543a5369e2affee5970
Shaan <22236218+ShaanCoding@users.noreply.github.com>
2024-05-13 19:52:24 +1000

    Generated New Frontend Types
    - Generated Frontend Types for Contributor
    
    Updated Frontend types again

---
+++ frontend/api/generated/models/IContributor.ts
@@ -0,0 +1,20 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type IContributor = {
+    name: string;
+    image: string;
+    title: string;
+    description: string;
+    url: Array<{
+        _type: 'Facebook' | 'Instagram' | 'Twitter' | 'Github' | 'LinkedIn' | 'Other';
+        url: string;
+    }>;
+    contributions: Array<{
+        name: string;
+        url: string;
+        type: ('OpenIssue' | 'ClosedIssueDone' | 'ClosedIssueCancelled' | 'PullRequestOpen' | 'PullRequestMerged' | 'PullRequestCancelled' | 'PullRequestDraft');
+    }>;
+};
+
