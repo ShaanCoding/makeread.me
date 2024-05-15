72f12ceade063bc6e89208f637a3a1f1f6f56262
Shaan <22236218+ShaanCoding@users.noreply.github.com>
2024-05-15 19:35:48 +1000

    Added Drag Trigger & Added Show Animations

--- frontend/components/GenerateTemplate/MainContent/Editor/Editor.tsx
+++ frontend/components/GenerateTemplate/MainContent/Editor/Editor.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { IFunction } from "@/api/generated"
 import { Reorder } from "framer-motion"
 import { FormProvider, SubmitHandler, useForm } from "react-hook-form"
@@ -16,7 +16,7 @@ const Editor = ({
   setVariables: React.Dispatch<React.SetStateAction<Record<string, any>>>
 }) => {
   const methods = useForm()
-  const { register, control, handleSubmit, watch } = methods
+  const { control, handleSubmit, watch } = methods
 
   const onSubmit: SubmitHandler<any> = (data) => {
     setVariables(data)
@@ -27,54 +27,35 @@ const Editor = ({
     return () => subscription.unsubscribe()
   }, [handleSubmit, watch])
 
-  const moveBlockUp = (index: number) => {
-    if (index === 0) return
-    const newBlocks = [...templateBlocks]
-    const temp = newBlocks[index]
-    newBlocks[index] = newBlocks[index - 1]
-    newBlocks[index - 1] = temp
-    setTemplateBlocks(newBlocks)
-  }
-
-  const moveBlockDown = (index: number) => {
-    if (index === templateBlocks.length - 1) return
-
-    const newBlocks = [...templateBlocks]
-    const temp = newBlocks[index]
-    newBlocks[index] = newBlocks[index + 1]
-    newBlocks[index + 1] = temp
-    setTemplateBlocks(newBlocks)
-  }
-
   const deleteBlock = (index: number) => {
     const newBlocks = [...templateBlocks]
     newBlocks.splice(index, 1)
     setTemplateBlocks(newBlocks)
   }
 
+
   return (
     <div className="grid grid-cols-1 gap-6 p-6 xl:w-full">
       <FormProvider {...methods}>
-        <Reorder.Group values={templateBlocks} onReorder={setTemplateBlocks}>
-          <form
-            onChange={handleSubmit(onSubmit)}
-            onSubmit={(e) => e.preventDefault()}
+        <form
+          onChange={handleSubmit(onSubmit)}
+          onSubmit={(e) => e.preventDefault()}
+        >
+          <Reorder.Group
             className="grid grid-cols-1 gap-6"
-          >
+            axis="y"
+            onReorder={setTemplateBlocks}
+            values={templateBlocks}>
             {templateBlocks.map((block: IFunction, index: number) => (
-              <Reorder.Item value={block} key={block.name}>
-                <EditorBlock
-                  block={block}
-                  moveBlockUp={() => moveBlockUp(index)}
-                  moveBlockDown={() => moveBlockDown(index)}
-                  deleteBlock={() => deleteBlock(index)}
-                  key={index}
-                  control={control}
-                />
-              </Reorder.Item>
+              <EditorBlock
+                block={block}
+                deleteBlock={() => deleteBlock(index)}
+                key={block.name}
+                control={control}
+              />
             ))}
-          </form>
-        </Reorder.Group>
+          </Reorder.Group>
+        </form>
       </FormProvider>
     </div>
   )
