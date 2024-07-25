a7fc8c91561f5af783c5c0b0e2ff83a58de7dcfe
Zekii <zach7505@gmail.com>
2024-07-25 14:29:18 +1000

    refactor: moved site logo out of nav items

--- frontend/components/site-header.tsx
+++ frontend/components/site-header.tsx
@@ -4,11 +4,16 @@ import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
 import { Icons } from "@/components/icons"
 import { MainNav } from "@/components/main-nav"
+import Image from "next/image"
 
 export function SiteHeader() {
   return (
     <header className="bg-background/35 sticky top-0 z-40 w-full border-b">
       <div className="mx-6 flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+        <Link href="/" className="flex items-center space-x-2 pr-6 md:pr-10">
+          <Image src="/logo.svg" alt="Logo" width={30} height={30} />
+          <span className="inline-block font-bold">{siteConfig.name}</span>
+        </Link>
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="hidden items-center space-x-1 sm:flex">
